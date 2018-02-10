04cf9392e73099190a8bbcc670f5dbc3a1e83bad
fredi <fredi.sa@hotmail.de>
2018-02-10 21:49:22 +0100

    Comment and USer interface DB

--- storage/storage.go
+++ storage/storage.go
@@ -1,11 +1,22 @@
 package storage
 
 import (
-	"time"
 	"image"
+	"time"
 )
 
 type Service interface {
 	CreateEvent(theme, starter, mainDish, dessert, infotext string, date time.Time, img image.Image) error
 	CreateParticipant(name string, event, menu int) error
+}
+
+type Comment struct {
+	Content string
+	Name    string
+}
+
+type User struct {
+	Name     string
+	Salt     string
+	Password string
 }
