b77e2e8880cc42a2a63dd85c8597bbf00e6cc8d0
Karsten Koehler <karsten.koehler95@gmail.com>
2018-02-10 21:45:40 +0100

    create Event and Participant types

--- storage/storage.go
+++ storage/storage.go
@@ -1,11 +1,31 @@
 package storage
 
 import (
-	"time"
 	"image"
+	"time"
 )
 
 type Service interface {
 	CreateEvent(theme, starter, mainDish, dessert, infotext string, date time.Time, img image.Image) error
 	CreateParticipant(name string, event, menu int) error
+	CreateComment(name, content string, event int) error
+	CreateImage(img image.Image, event int) error
+	CreateUser(name, password string) error
+
+	CheckCredentials(name, password string) (bool, error)
+}
+
+type Event struct {
+	Theme    string
+	Starter  string
+	MainDish string
+	Dessert  string
+	InfoText string
+	Date     time.Time
+	Img      image.Image
+}
+
+type Participant struct {
+	Name string
+	Menu int
 }
