a1a8c5446999b009cfc29661f949b5c080b01923
fredi <fredi.sa@hotmail.de>
2018-02-10 22:48:37 +0100

    started to implement Create Event,Participant, Comment and Image in mysql

--- storage/mydb/mysql.go
+++ storage/mydb/mysql.go
@@ -9,12 +9,17 @@ import (
 )
 
 const (
-	dbCreateUser = `INSERT INTO user ( name, salt, password) VALUES (?,?,?)`
+	dbCreateUser        = `INSERT INTO user ( name, salt, password) VALUES (?,?,?);`
+	dbCreateEvent       = `INSERT INTO event (theme, event_date, created, starter, main_dish, dessert, infotext, image) VALUES (?,?, created = NOW(),?,?,?,?,?)`
+	dbCreateParticipant = `INSERT INTO participant (name, created, menu, event_id) VALUES (?,created = Now(), ?, event_id = (SELECT id FROM Event ORDER BY  id LIMIT 1)) `
+	dbCreateComment     = `INSERT INTO comment (content, name, created, event_id) VALUES (?,?, created=Now(),event_id = (SELECT id FROM Event ORDER BY  id LIMIT 1))`
+	dbCreateImage       = `INSERT INTO images (event_id, picture) VALUES (event_id=(SELECT id FROM Event ORDER BY  id LIMIT 1), ?)`
 )
 
 var (
 	ErrUserAlreadyAssigned = fmt.Errorf("username already assigned")
 	ErrUserToLong          = fmt.Errorf("username must be less than 256 characters")
+	ErrInputToLong         = fmt.Errorf("some attribute was to long")
 	//ErrUserNotFound        = fmt.Errorf("user not found")
 )
 
@@ -27,19 +32,47 @@ func New() storage.Service {
 }
 
 func (c *connection) CreateEvent(event storage.Event) error {
-	panic("implement me")
+	_, err := c.db.Exec(dbCreateEvent, event.Theme, event.Date, event.Starter, event.MainDish, event.Dessert, event.InfoText, event.Img)
+	if msqlErr, ok := err.(*mysql.MySQLError); ok {
+		if msqlErr.Number == 1406 {
+			return ErrInputToLong
+		}
+		return fmt.Errorf("cannot execute statement: %v", err)
+	}
+	return err
 }
 
 func (c *connection) CreateParticipant(participant storage.Participant) error {
-	panic("implement me")
+	_, err := c.db.Exec(dbCreateParticipant, participant.Name, participant.Menu)
+	if msqlErr, ok := err.(*mysql.MySQLError); ok {
+		if msqlErr.Number == 1406 {
+			return ErrInputToLong
+		}
+		return fmt.Errorf("cannot execute statement: %v", err)
+	}
+	return err
 }
 
 func (c *connection) CreateComment(comment storage.Comment) error {
-	panic("implement me")
+	_, err := c.db.Exec(dbCreateComment, comment.Content, comment.Name)
+	if msqlErr, ok := err.(*mysql.MySQLError); ok {
+		if msqlErr.Number == 1406 {
+			return ErrInputToLong
+		}
+		return fmt.Errorf("cannot execute statement: %v", err)
+	}
+	return err
 }
 
 func (c *connection) CreateImage(img image.Image, event int) error {
-	panic("implement me")
+	_, err := c.db.Exec(dbCreateImage, img)
+	if msqlErr, ok := err.(*mysql.MySQLError); ok {
+		if msqlErr.Number == 1406 {
+			return ErrInputToLong
+		}
+		return fmt.Errorf("cannot execute statement: %v", err)
+	}
+	return err
 }
 
 func (c *connection) CreateUser(name, password string) error {
