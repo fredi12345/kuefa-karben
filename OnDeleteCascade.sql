e440d1b9b49ced84d7aaed970d98f4d29110a626
Fredi <fredi.sa@hotmail.de>
2019-02-21 21:58:40 +0100

    on delete cascade

--- OnDeleteCascade.sql
+++ OnDeleteCascade.sql
@@ -1,4 +1,21 @@
-## Erst die Statements unten, dann EINMAL diese:
+## Nur die nehmen, die auch vorhanden sind (die die nicht gelb hinterlegt sind)....
+ALTER TABLE participant DROP FOREIGN KEY participant_ibfk_1;
+ALTER TABLE images DROP FOREIGN KEY images_ibfk_1;
+ALTER TABLE comment DROP FOREIGN KEY comment_ibfk_1;
+ALTER TABLE participant DROP FOREIGN KEY participant_ibfk_2;
+ALTER TABLE images DROP FOREIGN KEY images_ibfk_2;
+ALTER TABLE comment DROP FOREIGN KEY comment_ibfk_2;
+ALTER TABLE participant DROP FOREIGN KEY participant_ibfk_3;
+ALTER TABLE images DROP FOREIGN KEY images_ibfk_3;
+ALTER TABLE comment DROP FOREIGN KEY comment_ibfk_3;
+ALTER TABLE participant DROP FOREIGN KEY participant_ibfk_4;
+ALTER TABLE images DROP FOREIGN KEY images_ibfk_4;
+ALTER TABLE comment DROP FOREIGN KEY comment_ibfk_4;
+ALTER TABLE participant DROP FOREIGN KEY participant_ibfk_5;
+ALTER TABLE images DROP FOREIGN KEY images_ibfk_5;
+ALTER TABLE comment DROP FOREIGN KEY comment_ibfk_5;
+
+## Erst die Statements oben, dann EINMAL diese:
 ALTER TABLE participant
 ADD FOREIGN KEY (event_id)
 REFERENCES event(event_id)
@@ -13,26 +30,6 @@ ALTER TABLE comment
 ADD FOREIGN KEY (event_id)
 REFERENCES event(event_id)
 ON DELETE CASCADE;
-
-## Nur die nehmen, die auch vorhanden sind (die die nicht gelb hinterlegt sind)....
-ALTER TABLE participant DROP FOREIGN KEY participant_ibfk_1;
-ALTER TABLE participant DROP FOREIGN KEY participant_ibfk_2;
-ALTER TABLE participant DROP FOREIGN KEY participant_ibfk_3;
-ALTER TABLE participant DROP FOREIGN KEY participant_ibfk_4;
-ALTER TABLE participant DROP FOREIGN KEY participant_ibfk_5;
-
-ALTER TABLE images DROP FOREIGN KEY images_ibfk_1;
-ALTER TABLE images DROP FOREIGN KEY images_ibfk_2;
-ALTER TABLE images DROP FOREIGN KEY images_ibfk_3;
-ALTER TABLE images DROP FOREIGN KEY images_ibfk_4;
-ALTER TABLE images DROP FOREIGN KEY images_ibfk_5;
-
-ALTER TABLE comment DROP FOREIGN KEY comment_ibfk_1;
-ALTER TABLE comment DROP FOREIGN KEY comment_ibfk_2;
-ALTER TABLE comment DROP FOREIGN KEY comment_ibfk_3;
-ALTER TABLE comment DROP FOREIGN KEY comment_ibfk_4;
-ALTER TABLE comment DROP FOREIGN KEY comment_ibfk_5;
-
 
 ## Zeigt Version an:
 SELECT @@Version;
