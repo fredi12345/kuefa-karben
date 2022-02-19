2af8b9187197eb0da6dcbb27c97145d30d4b1cd7
Lackmake <kevinlackmann.kl@gmail.com>
2022-02-19 22:18:57 +0100

    basic header style

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -11,12 +11,17 @@ useRouter()
 </script>
 
 <style>
+:root{
+  --theme-color: #8B0000FF;
+}
+
+body{
+  background: #ddd;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- frontend/src/components/NavigationBar.vue
+++ frontend/src/components/NavigationBar.vue
@@ -1,4 +1,7 @@
 <template>
+  <header>
+    <img alt="Küfa Logo" src="../assets/logo_wide_thin.png">
+  </header>
   <nav class="navigation">
     <router-link to="/">{{ $t('navigation.home') }}</router-link>
     <router-link to="/events">{{ $t('navigation.events') }}</router-link>
@@ -11,5 +14,35 @@
 </script>
 
 <style scoped lang="scss">
+header img {
+  width: 100%;
+  height: 20vh;
+  filter: drop-shadow(4px 4px 3px black);
+  object-fit: cover;
+}
 
+nav {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+  padding: 10px;
+  border-radius: 10px;
+  background-color: var(--theme-color);
+  box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23);
+  text-shadow: 1px 1px 1px rgba(0 0 0 / 0.5);
+}
+
+nav a {
+  padding: 8px 16px;
+  transition: background-color 0.15s;
+  text-decoration: none;
+  text-transform: uppercase;
+  color: white;
+  border-radius: 5px;
+}
+
+nav a:hover, nav a.router-link-active {
+  transition: background-color 0.30s;
+  background-color: rgba(255, 255, 255, 0.2);
+}
 </style>
